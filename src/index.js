b4a278aaf050175bda306abb3ee8f68c0a4bdd39
alon bar david <illniyar@gmail.com>
2017-04-07 11:47:37 +0300

    fix: missing files in compilation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import merge from './internal/merge';
 import mount from './internal/mount';
-import {mountDef} from './internal/advanced';
 
 
-export {merge,mount,mountDef};
+export {merge,mount};
 
--- src/saga.js
+++ src/saga.js
@@ -1,5 +1,4 @@
 import {merge,mount} from './internal/saga';
-import {mountDef} from './internal/saga/advanced';
 
-export {merge,mount,mountDef};
+export {merge,mount};
 
